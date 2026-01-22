c177a0e31ad2320f558453851bf3c2f49eefd998
hanson777 <154561030+hanson777@users.noreply.github.com>
2026-01-21 21:07:17 -0800

    feat: basic concurrency with pthread and atomics

--- server.c
+++ server.c
@@ -1,6 +1,9 @@
 #include <errno.h>
 #include <netdb.h>
+#include <pthread.h>
+#include <stdatomic.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,41 +13,24 @@ struct http_request {
   char method[16];
   char path[256];
   char version[16];
-  char headers[3][256];
+  char headers[16][256];
   int header_count;
   char *body;
 };
 
+atomic_int request_count = 0;
+
 void parse_request(char *buffer, struct http_request *req) {
   char *line;
   char *buf = buffer;
-  char *method;
-  char *path;
-  char *version;
+
   line = strsep(&buf, "\r\n");
-
-  // get the method
-  method = strsep(&line, "/");
-  strncpy(req->method, method, sizeof(req->method) - 1);
-  req->method[sizeof(req->method) - 1] = '\0';
-  printf("Method: %s\n", req->method);
-
-  // get path
-  path = strsep(&line, " ");
-  strncpy(req->path, path, sizeof(req->path) - 1);
-  req->path[sizeof(req->path) - 1] = '\0';
-  printf("Path: %s\n", req->path);
-
-  // get version
-  version = line;
-  strncpy(req->version, version, sizeof(req->version) - 1);
-  printf("Version: %s\n", version);
+  sscanf(line, "%15s %255s %15s", req->method, req->path, req->version);
 
   // get rest of headers
   int header_count = 0;
   while ((line = strsep(&buf, "\r\n")) != NULL) {
     if (strlen(line) > 0) {
-      printf("%s\n", line);
       strncpy(req->headers[header_count], line,
               sizeof(req->headers[header_count]) - 1);
       req->headers[header_count][sizeof(req->headers[header_count]) - 1] = '\0';
@@ -54,16 +40,60 @@ void parse_request(char *buffer, struct http_request *req) {
   req->header_count = header_count;
 }
 
+void *handle_client(void *client) {
+  atomic_fetch_add(&request_count, 1);
+  int clientfd = *(int *)(client);
+  free(client);
+  printf("[thread %lu] START client=%d\n", (unsigned long)pthread_self(),
+         clientfd);
+
+  char buf[1024];
+  int bytes = recv(clientfd, buf, sizeof(buf) - 1, 0);
+  if (bytes == -1) {
+    perror("recv");
+  } else if (bytes == 0) {
+    printf("Client disconnected\n");
+  } else {
+    buf[bytes] = '\0';
+  }
+
+  struct http_request req;
+  parse_request(buf, &req);
+
+  printf("[thread %lu] %s %s\n", (unsigned long)pthread_self(), req.method,
+         req.path);
+
+  printf("Requests received: %d\n", atomic_load(&request_count));
+
+  char response[2048];
+  if (strcmp(req.path, "/slow") == 0) {
+    printf("[thread %lu] ENTER /slow\n", (unsigned long)pthread_self());
+
+    sleep(5);
+
+    printf("[thread %lu] EXIT /slow\n", (unsigned long)pthread_self());
+
+    strncpy(response, "HTTP/1.1 200 OK\r\n\r\nSlow response\n",
+            sizeof(response) - 1);
+    response[sizeof(response) - 1] = '\0';
+  } else if (strcmp(req.path, "/fast") == 0) {
+    strncpy(response, "HTTP/1.1 200 OK\r\n\r\nFast response\n",
+            sizeof(response) - 1);
+    response[sizeof(response) - 1] = '\0';
+  }
+
+  send(clientfd, response, strlen(response), 0);
+  printf("[thread %lu] END client=%d\n", (unsigned long)pthread_self(),
+         clientfd);
+
+  close(clientfd);
+  return NULL;
+}
+
 int main() {
   struct addrinfo hints;
   struct addrinfo *res;
   int backlog = 10;
-
-  char buffer[] = "GET /about HTTP/1.1\r\n"
-                  "Host: localhost:8080\r\n"
-                  "User-Agent: curl/7.68.0\r\n"
-                  "Accept: */*\r\n"
-                  "\r\n";
 
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
@@ -91,29 +121,20 @@ int main() {
     perror("listen");
     return 1;
   }
-
   printf("Server successfully started at :8080\n");
-  int clientfd = accept(desc, NULL, NULL);
-  if (clientfd == -1) {
-    perror("accept");
-    return 1;
-  }
 
-  char buf[1024];
-  int bytes = recv(clientfd, buf, sizeof(buf) - 1, 0);
-  if (bytes == -1) {
-    perror("recv");
-    return 1;
-  } else if (bytes == 0) {
-    printf("Client disconnected\n");
-  } else {
-    buf[bytes] = '\0';
-    printf("Received: %s\n", buf);
+  while (1) {
+    int clientfd = accept(desc, NULL, NULL);
+    if (clientfd == -1) {
+      perror("accept");
+      continue;
+    }
+    pthread_t thread1;
+    int *clientfd_ptr = malloc(sizeof(int));
+    *clientfd_ptr = clientfd;
+    pthread_create(&thread1, NULL, handle_client, clientfd_ptr);
+    pthread_detach(thread1);
   }
-
-  struct http_request *req;
-  parse_request(buf, req);
-  close(clientfd);
 
   freeaddrinfo(res);
   return 0;
